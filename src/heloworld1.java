6ae787e13dc0c592fe8569ca27a6204d0002a82c
аслан мекеидзе <mekeidzeaslan9@gmail.com>
2022-02-10 15:14:20 +0300

    Initial commit

---
+++ src/heloworld1.java
@@ -0,0 +1,10 @@
+public class heloworld1 {
+    public static void main(String[] argd) {
+        int i= 5;
+        double b = 10.1;
+        double c = i + b;
+        double d = c * 11;
+        System.out.println("variable =" +d);
+
+    }
+}
